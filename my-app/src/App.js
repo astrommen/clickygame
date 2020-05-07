6bc4c98f8a7556062ec551cdb39aa57923fed993
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-05-07 18:04:23 -0400

    got hit to work lil, add github pages to homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import GameCard from "./components/GameCards";
@@ -6,47 +6,53 @@ import cards from "./cards.json";
 import './App.css';
 
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
 // shuffle(cards); console.log(cards);
 // let clicked = [];
 
-class App extends Component {
+class App extends React.Component {
   // initialiazing this.state.cards to cards.json array
   state = {
     cards: cards,
     score: 0,
+    hit: false
   };
 
+  shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
 
+  handleHit = _hit => {
+    this.setState({ hit: this.state.hit + 1 });
+  };
   // editScore = () => {
-  //   clicked.push(cards.id);
-
-  //   this.setState({ score: this.state.score + 1 });
-  // }
-  
-  render() {
-    shuffle(cards);
-    console.log(cards);
-    // this.state.cards.map(card);
-    // console.log(card);
-    return (
-      <Wrapper>
+    //   clicked.push(cards.id);
+    
+    //   this.setState({ score: this.state.score + 1 });
+    // }
+    
+    render() {
+      this.shuffle(this.state.cards);
+      console.log(cards);
+      // this.state.cards.map(card);
+      // console.log(card);
+      return (
+        <Wrapper>
         <Title>Adventure Time Click Game</Title>
+        <p className="card hit">Card Hit: {this.state.hit}</p>
         <div className="cardContainer">
 
         {this.state.cards.map(card => (
           <GameCard 
-            id={card.id}
-            key={card.id}
-            image={card.image}
-            hit={this.hit}
+          id={card.id}
+          key={card.id}
+          image={card.image}
+          handleHit= {this.handleHit}
+          // hit= {this.hit}
             />
+            
           ))}
         </div>
       </Wrapper>
--- my-app/src/components/GameCards/index.js
+++ my-app/src/components/GameCards/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+
 function GameCard(props) {
   return (
     <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+      <div className="img-container" >
+        <img alt={props.name} src={props.image} onClick={()=>{props.handleHit(props.hit)}} />
       </div>
     </div>
   );
 }
 
 export default GameCard;
+
